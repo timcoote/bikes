f430e9f28ab97f10b2cd0e1554f5b31e9d1c81cf
Tim Coote <tim+heroku.com@coote.org>
2014-04-13 19:40:16 +0000

    works with foreman locally on port 5000 (use vagrant -L 5000:localhost:5000)

--- hello.py
+++ hello.py
@@ -1,9 +1,36 @@
 import os
 from flask import Flask
+import json, urllib2
 
 app = Flask (__name__)
 
-@app.route ('/')
 
+
+
+#print json.dumps (data ["dockStation"], sort_keys=True, indent=4)
+
+#print u"\n".join ([ "%s, %s: %s, bikes Available %s, slots free %s" % (x [u'latitude'], x[u'longitude'], x[u'name'], x[u'bikesAvailable'], x[u'emptySlots']) for x in stations]).encode ('utf-8')
+
+@app.route ('/')
+def index ():
+    return ('hello dad')
+
+@app.route ('/stuff')
 def hello ():
-    return 'Hello Dad'
+    data = json.load (urllib2.urlopen ('http://api.bike-stats.co.uk/service/rest/bikestats?format=json'))
+    stations = data ["dockStation"]
+    js = open ("parta.html").read()
+#    return "stuff"
+    pb = open ("partb.html").read()
+    pc = open ("partc.html").read()
+    i = 1
+    for s in stations:
+        #print s
+        js += pb % tuple ([x.encode('utf-8') for x in s[u'name'].strip(), s[u'latitude'], s[u'longitude'], s[u'bikesAvailable'], s[u'emptySlots']])
+        i+=1
+        if i > 150: break
+    js += pc
+#    print js
+    return js
+
+
