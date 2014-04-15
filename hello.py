c33deeff6cc2186290b13fa6ce23e0d62aec263e
Tim Coote <tim+git@coote.org>
2014-04-15 19:15:31 +0100

    fewer points recovered. ordered around distance from cx

--- hello.py
+++ hello.py
@@ -1,15 +1,16 @@
 import os
+from collections import defaultdict
 from flask import Flask
 import json, urllib2
 
 app = Flask (__name__)
 
-
-
-
 #print json.dumps (data ["dockStation"], sort_keys=True, indent=4)
 
 #print u"\n".join ([ "%s, %s: %s, bikes Available %s, slots free %s" % (x [u'latitude'], x[u'longitude'], x[u'name'], x[u'bikesAvailable'], x[u'emptySlots']) for x in stations]).encode ('utf-8')
+
+def sep (here, there):
+    return (here[0]-there[0])**2 + (here[1] - there [1]) ** 2
 
 @app.route ('/')
 def index ():
@@ -19,18 +20,30 @@ def index ():
 def hello ():
     data = json.load (urllib2.urlopen ('http://api.bike-stats.co.uk/service/rest/bikestats?format=json'))
     stations = data ["dockStation"]
+    cx = (51.50741, -0.12725)
+    dists = defaultdict (list)
+    for s in stations:
+        loc=(float (s[u'latitude']), float (s['longitude']))
+        dist = sep (cx, loc)
+    #    print loc, s[u'name'].encode ('utf-8'), dist
+        dists [dist].append (s)
     js = open ("parta.html").read()
 #    return "stuff"
     pb = open ("partb.html").read()
     pc = open ("partc.html").read()
     i = 1
-    for s in stations:
-        #print s
+#    print "here come the dists", dists
+    for (d, sr) in sorted(dists.items (), key = lambda x: x[0], reverse = False):
+#        print d, sr
+        s = sr [0]
         js += pb % tuple ([x.encode('utf-8') for x in s[u'name'].strip(), s[u'latitude'], s[u'longitude'], s[u'bikesAvailable'], s[u'emptySlots']])
         i+=1
-        if i > 150: break
+        if i > 15: break
     js += pc
 #    print js
     return js
 
+##
+if __name__ == "__main__":
+    print hello()
 
