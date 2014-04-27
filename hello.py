5eac4676d8d538539d354f8ca9f80177b553a070
Tim Coote <tim+git@coote.org>
2014-04-27 10:01:15 +0100

    this works but has json literal in x.html (called as /test)

--- hello.py
+++ hello.py
@@ -1,45 +1,61 @@
 import os
 from collections import defaultdict
-from flask import Flask
+from flask import Flask, Response, url_for, send_from_directory
 import json, urllib2
 from math import cos, pi
 
-app = Flask (__name__)
+app = Flask (__name__, static_folder='static', static_url_path='')
 
 #print json.dumps (data ["dockStation"], sort_keys=True, indent=4)
 
 #print u"\n".join ([ "%s, %s: %s, bikes Available %s, slots free %s" % (x [u'latitude'], x[u'longitude'], x[u'name'], x[u'bikesAvailable'], x[u'emptySlots']) for x in stations]).encode ('utf-8')
+
+dists = defaultdict (list)
 
 def sep (here, there):
 # scale lat so that one deg lat = 1 deg long
     londonRatio = cos (51.0/90.0 * pi/2.0)
     return ((here[0]-there[0])/londonRatio)**2 + (here[1] - there [1]) ** 2
 
+@app.route ('/static/fullmap.html')
+# just spit out a map that was made before
+def stuff1 ():
+#    return url_for ('static', filename='fullmap.html')
+    return send_from_directory ('static', filename='blah.html')
+
 @app.route ('/')
 def index ():
     return ('hello dad')
 
+@app.route ('/test')
+def tester ():
+    return (open ('x.html').read())
+    
 @app.route ('/stuff')
 def hello ():
     return loc1 (51.50741, -0.12725)
 
 @app.route ('/loc/<float:lat>/<float:long>')
 def location (lat, long):
-    return loc1 (lat, long)
+    return loc2 (lat, long)
 
 @app.route ('/bikes/<float:lat>/<float:long>')
 def stns (lat, long):
     return loc (lat, long)
+
+@app.route ('/json')
+def retjson ():
+    return Response('{"s": "hello"}', mimetype='application/json')
 
 def loc1 (lat, long):
     js = open ("parta.html").read()
     return js + loc (lat, long) + open ("partc.html").read ()
 
 def loc (lat, long):
+    global dists
     data = json.load (urllib2.urlopen ('http://bike-stats.appspot.com/service/rest/bikestats?format=json'))
     stations = data ["dockStation"]
     cx = (lat, long)
-    dists = defaultdict (list)
     for s in stations:
         loc=(float (s[u'latitude']), float (s['longitude']))
         dist = sep (cx, loc)
@@ -58,7 +74,36 @@ def loc (lat, long):
 #    print js
     return js
 
+def u (val):
+    return val.encode('utf-8')
+
+def loc2 (lat, long):
+    global dists
+    data = json.load (urllib2.urlopen ('http://bike-stats.appspot.com/service/rest/bikestats?format=json'))
+    stations = data ["dockStation"]
+    cx = (lat, long)
+    markers = []
+    for s in stations:
+        loc=(float (s[u'latitude']), float (s['longitude']))
+        dist = sep (cx, loc)
+    #    print loc, s[u'name'].encode ('utf-8'), dist
+        dists [dist].append (s)
+    js = ""
+    pb = open ("partb.html").read()
+    i = 1
+#    print "here come the dists", dists
+    for (d, sr) in sorted(dists.items (), key = lambda x: x[0], reverse = False):
+#        print d, sr
+        s = sr [0]
+        marker = {"name": u(s[u'name'].strip()), "loc": {"lat": u(s[u'latitude']), "long":u(s[u'longitude'])}, "levels": {"available": u(s[u'bikesAvailable']), "free": u(s[u'emptySlots'])}}
+#        markers.append (json.dumps (marker))
+        markers.append (marker)
+        i+=1
+        if i > 150: break
+#    print js
+    return markers
+
 #
 if __name__ == "__main__":
-    print hello()
+    print loc2 (51.50741, -0.12725)
 
