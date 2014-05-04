107b45dcf0d02f90e518b12ba9d70f01d9d0fa01
Tim Coote <tim+heroku.com@coote.org>
2014-05-04 15:06:15 +0000

    adding in the amqpnode stuff

---
+++ static/squaremo-amqp.node-5d4bcc4/callback_api.js
@@ -0,0 +1,19 @@
+var raw_connect = require('./lib/connect').connect;
+var CallbackModel = require('./lib/callback_model').CallbackModel;
+
+// Supports three shapes:
+// connect(url, options, callback)
+// connect(url, callback)
+// connect(callback)
+function connect(url, options, cb) {
+  if (typeof url === 'function')
+    cb = url, url = false, options = false;
+  else if (typeof options === 'function')
+    cb = options, options = false;
+
+  raw_connect(url, options).then(function(c) {
+    cb(null, new CallbackModel(c));
+  }, cb);
+};
+
+module.exports.connect = connect;
